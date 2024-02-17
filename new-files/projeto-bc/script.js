384f6beb0e045cff6ef0062046be1048034c92bf
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2024-02-17 14:34:30 -0300

    etapa-17.02

--- new-files/projeto-bc/script.js
+++ new-files/projeto-bc/script.js
@@ -1,2 +1,27 @@
 //por meio do JS, será criada função para quando ocorrer o click no índice econômico automaticamente puxar caixa com conteúdo referente ao mesmo.
 
+const Inflação = {
+    textInicio: "O índice de inflação esperado para 2023 ao início do ano ficou em 5,36%",
+    textFinal: "O índice de inflação finalizado em 2023 ficou em 4,46%"
+}
+
+const PIB = {
+    textInicio: "O PIB esperado para 2023 ao início do ano ficou em 0,78%",
+    textFinal: "O PIB finalizado em 2023 ficou em 2,92%"
+}
+
+const Câmbio = {
+    textInicio: "O Câmbio esperado para 2023 ao início do ano ficou em R$ 5,28",
+    textFinal: "O Câmbio finalizado em 2023 ficou em R$ 4,90"
+}
+
+const Selic = {
+    textInicio: "A Selic esperada para 2023 ao início do ano ficou em 12,25%",
+    textFinal: "A Selic finalizada em 2023 ficou em 11,75%"
+}
+
+const selecaoMedidas = [Inflação, PIB, Câmbio, Selic]
+
+function selecaoIndice() {
+    
+}
