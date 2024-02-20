c381f1a5938898f0080c01d28af31f726a938256
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2024-02-19 21:52:11 -0300

    etapa-19.02

--- new-files/projeto-bc/script.js
+++ new-files/projeto-bc/script.js
@@ -1,27 +1,32 @@
 //por meio do JS, será criada função para quando ocorrer o click no índice econômico automaticamente puxar caixa com conteúdo referente ao mesmo.
 
+const connect = document.getElementById('text-connection')
+
+//as const's abaixo contêm os respectivos textos de cada índice econômico.
+
 const Inflação = {
-    textInicio: "O índice de inflação esperado para 2023 ao início do ano ficou em 5,36%",
-    textFinal: "O índice de inflação finalizado em 2023 ficou em 4,46%"
+    text: "O índice de inflação esperado para 2023 ao início do ano foi 5,36%. Entretanto, finalizou 2023 em 4,46%."
 }
 
 const PIB = {
-    textInicio: "O PIB esperado para 2023 ao início do ano ficou em 0,78%",
-    textFinal: "O PIB finalizado em 2023 ficou em 2,92%"
+    text: "O PIB esperado para 2023 ao início do ano ficou em 0,78%. Entretanto, finalizou 2023 em 2,92%."
 }
 
 const Câmbio = {
-    textInicio: "O Câmbio esperado para 2023 ao início do ano ficou em R$ 5,28",
-    textFinal: "O Câmbio finalizado em 2023 ficou em R$ 4,90"
+    text: "O Câmbio esperado para 2023 ao início do ano ficou em R$ 5,28. Entretanto, finalizou 2023 em R$ 4,90."
 }
 
 const Selic = {
-    textInicio: "A Selic esperada para 2023 ao início do ano ficou em 12,25%",
-    textFinal: "A Selic finalizada em 2023 ficou em 11,75%"
+    text: "A Selic esperada para 2023 ao início do ano ficou em 12,25%. Entretanto, finalizou 2023 em 11,75%."
 }
 
 const selecaoMedidas = [Inflação, PIB, Câmbio, Selic]
 
+//a função abaixo cria const para que possa capturar o input selecionado e automaticamente gerar o texto respectivo das const's acima.
+
 function selecaoIndice() {
-    
+    const opcaoEcoIndice = document.querySelector('[name="index"]:checked').id.charAt(0);
+    ecoIndice = opcaoEcoIndice;
+
+    connect.innerHTML = selecaoMedidas[ecoIndice].text
 }
