f033a5a6227359a2cf2a21ee2836fc04f6c7a2e7
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2024-02-25 13:51:58 -0300

    etapa-25.02.1

--- new-files/projeto-users/script.js
+++ new-files/projeto-users/script.js
@@ -1,15 +1,4 @@
-const selecao = document.getElementById('selecao').value;
-const resposta = document.getElementById('resposta');
-
-function Dados() {
-    if (selecao == 'George') {
-        resposta.innerHTML = selecao
-    }
-}
-
-function fetchData {
 fetch('https://reqres.in/api/users')
 .then(response => response.json())
-.then((data) => console.log(data))
+.then((data) => console.log(data.data[5].last_name))
 .catch((error) => console.log('error'))
-}
